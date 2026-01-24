fca2d4962b817e0d176ce88a636d1bf7f1d7b849
guntisdev <guntis.smaukstelis@gmail.com>
2026-01-24 21:29:16 +0200

    Add newCommand and genCommand tests

--- cmd/entlite/main_test.go
+++ cmd/entlite/main_test.go
@@ -1,12 +1,48 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"os"
+	"testing"
+)
 
-func TestBasic(t *testing.T) {
-	expected := 1
-	actual := 1
+func TestNewCommandFunction(t *testing.T) {
+	tmpDir := t.TempDir()
+	oldWd, _ := os.Getwd()
+	defer os.Chdir(oldWd)
+	os.Chdir(tmpDir)
 
-	if actual != expected {
-		t.Errorf("Expected %d, but got %d", expected, actual)
+	newCommand([]string{"User", "Post"})
+
+	expectedFiles := []string{
+		"ent/schema/user.go",
+		"ent/schema/post.go",
+		"ent/gen/generate.go",
+		"ent/sqlc.yaml",
+		"ent/buf.yaml",
+		"ent/buf.gen.yaml",
+	}
+
+	for _, file := range expectedFiles {
+		if _, err := os.Stat(file); os.IsNotExist(err) {
+			t.Errorf("Expected file %s was not created", file)
+		}
+	}
+}
+
+func TestGenCommandFunction(t *testing.T) {
+	oldStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	genCommand([]string{})
+
+	w.Close()
+	os.Stdout = oldStdout
+	output, _ := io.ReadAll(r)
+
+	expected := "implement genCommand\n"
+	if string(output) != expected {
+		t.Errorf("Expected %q, got %q", expected, string(output))
 	}
 }
