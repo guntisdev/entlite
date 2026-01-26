bcb43ec3b4427d83df4a60c756031e08cd26d52c
guntisdev <guntis.smaukstelis@gmail.com>
2026-01-26 18:37:02 +0200

    Builder pattern for bool and int32

--- pkg/entlite/fields.go
+++ pkg/entlite/fields.go
@@ -54,23 +54,77 @@ func (f *StringField) ProtoField(num int) StringFieldBuilder {
 }
 
 // --------------------------------- bool ---------------------------------
+type BoolFieldBuilder interface {
+	Default(bool) BoolFieldBuilder
+	ProtoField(int) BoolFieldBuilder
+
+	field()
+}
+
 type BoolField struct {
-	name string
+	name       string
+	defaultVal *bool
+	protoField *int
 }
 
-func (BoolField) field() {}
+func (*BoolField) field() {}
 
-func Bool(name string) BoolField {
-	return BoolField{name: name}
+func Bool(name string) BoolFieldBuilder {
+	return &BoolField{name: name}
 }
 
-// --------------------------------- bool ---------------------------------
+func (f *BoolField) GetDefault() *bool {
+	return f.defaultVal
+}
+
+func (f *BoolField) GetProtoField() *int {
+	return f.protoField
+}
+
+func (f *BoolField) Default(value bool) BoolFieldBuilder {
+	f.defaultVal = &value
+	return f
+}
+
+func (f *BoolField) ProtoField(num int) BoolFieldBuilder {
+	f.protoField = &num
+	return f
+}
+
+// --------------------------------- int32 ---------------------------------
+type Int32FieldBuilder interface {
+	Default(int32) Int32FieldBuilder
+	ProtoField(int) Int32FieldBuilder
+
+	field()
+}
+
 type Int32Field struct {
-	name string
+	name       string
+	defaultVal *int32
+	protoField *int
 }
 
-func (Int32Field) field() {}
+func (*Int32Field) field() {}
 
-func Int32(name string) Int32Field {
-	return Int32Field{name: name}
+func Int32(name string) Int32FieldBuilder {
+	return &Int32Field{name: name}
+}
+
+func (f *Int32Field) GetDefault() *int32 {
+	return f.defaultVal
+}
+
+func (f *Int32Field) GetProtoField() *int {
+	return f.protoField
+}
+
+func (f *Int32Field) Default(value int32) Int32FieldBuilder {
+	f.defaultVal = &value
+	return f
+}
+
+func (f *Int32Field) ProtoField(num int) Int32FieldBuilder {
+	f.protoField = &num
+	return f
 }
