5a0a90bb163a8efec9811aca64d70bb00f6c60fe
Guntis <guntis.smaukstelis@gmail.com>
2026-01-26 19:42:40 +0200

    Types for entity service and methods
    * Add Service and Method types
    
    * Add MethodsAnnotation

--- pkg/entlite/schema.go
+++ pkg/entlite/schema.go
@@ -10,16 +10,46 @@ type Field interface {
 	field()
 }
 
-type Entity interface {
-	EntityName() string
-	EntityFields() []Field
-	EntityAnnotations() []Annotation
-}
-
+//	type Entity interface {
+//		EntityName() string
+//		EntityFields() []Field
+//		EntityAnnotations() []Annotation
+//	}
 type MessageAnnotation struct{}
 
 func (MessageAnnotation) annotation() {}
 
 func Message() Annotation {
 	return MessageAnnotation{}
+}
+
+type ServiceAnnotation struct{}
+
+func (ServiceAnnotation) annotation() {}
+
+func Service(annotations ...Annotation) Annotation {
+	return ServiceAnnotation{}
+}
+
+// CRUD method type
+type Method int
+
+const (
+	MethodCreate Method = 1 << iota
+	MethodGet
+	MethodUpdate
+	MethodDelete
+	MethodList
+)
+
+type MethodsAnnotation struct {
+	Methods []Method
+}
+
+func (MethodsAnnotation) annotation() {}
+
+func Methods(...Method) Annotation {
+	var methodList []Method
+
+	return MethodsAnnotation{Methods: methodList}
 }
