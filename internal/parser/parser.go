0436bbe5f8742209ef0d378c12a92378b9fb7a41
Guntis <guntis.smaukstelis@gmail.com>
2026-02-08 22:27:11 +0200

    Parse fields

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -5,6 +5,7 @@ import (
 	"go/ast"
 	"go/parser"
 	"go/token"
+	"strings"
 
 	"github.com/guntisdev/entlite/internal/schema"
 )
@@ -62,6 +63,7 @@ func parseEntityFromFile(discovered DiscoveredEntity) (schema.Entity, error) {
 			continue
 		}
 
+		// Parse Annotations
 		if funcDecl.Name.Name == "Annotations" {
 			annotations, err := parseAnnotationsMethod(funcDecl)
 			if err != nil {
@@ -70,11 +72,159 @@ func parseEntityFromFile(discovered DiscoveredEntity) (schema.Entity, error) {
 			entity.Annotations = annotations
 		}
 
-		// parse Fields
+		// Parse Fields
+		if funcDecl.Name.Name == "Fields" {
+			fields, err := parseFieldsMethod(funcDecl)
+			if err != nil {
+				return entity, fmt.Errorf("failed to parse fields: %w", err)
+			}
+			entity.Fields = fields
+		}
 
 	}
 
 	return entity, nil
+}
+
+func parseFieldsMethod(funcDecl *ast.FuncDecl) ([]schema.Field, error) {
+	var fields []schema.Field
+
+	if funcDecl.Body == nil {
+		return fields, nil
+	}
+
+	for _, stmt := range funcDecl.Body.List {
+		retStmt, ok := stmt.(*ast.ReturnStmt)
+		if !ok {
+			continue
+		}
+
+		for _, result := range retStmt.Results {
+			if compLit, ok := result.(*ast.CompositeLit); ok {
+				for _, elt := range compLit.Elts {
+					field := parseFieldExpression(elt)
+					if field.Name != "" {
+						fields = append(fields, field)
+					}
+				}
+
+			}
+		}
+	}
+
+	return fields, nil
+}
+
+func parseFieldExpression(expr ast.Expr) schema.Field {
+	field := schema.Field{}
+
+	// Handle method chaining like entlite.String("name").ProtoField(2)
+	currentExpr := expr
+
+	for currentExpr != nil {
+		switch e := currentExpr.(type) {
+		case *ast.CallExpr:
+			if selExpr, ok := e.Fun.(*ast.SelectorExpr); ok {
+				methodName := selExpr.Sel.Name
+
+				switch methodName {
+				case "String":
+					field.Type = schema.FieldTypeString
+					if len(e.Args) > 0 {
+						if lit, ok := e.Args[0].(*ast.BasicLit); ok && lit.Kind == token.STRING {
+							field.Name = strings.Trim(lit.Value, "\"")
+						}
+					}
+				case "Int32":
+					field.Type = schema.FieldTypeInt32
+					if len(e.Args) > 0 {
+						if lit, ok := e.Args[0].(*ast.BasicLit); ok && lit.Kind == token.STRING {
+							field.Name = strings.Trim(lit.Value, "\"")
+						}
+					}
+				case "Bool":
+					field.Type = schema.FieldTypeBool
+					if len(e.Args) > 0 {
+						if lit, ok := e.Args[0].(*ast.BasicLit); ok && lit.Kind == token.STRING {
+							field.Name = strings.Trim(lit.Value, "\"")
+						}
+					}
+					// TODO make time field
+					// case "Time":
+					//                 field.Type = schema.FieldTypeTime
+					// 	if len(e.Args) > 0 {
+					// 		if lit, ok := e.Args[0].(*ast.BasicLit); ok && lit.Kind == token.STRING {
+					// 			field.Name = strings.Trim(lit.Value, "\"")
+					// 		}
+					// 	}
+				case "ProtoField":
+					if len(e.Args) > 0 {
+						if lit, ok := e.Args[0].(*ast.BasicLit); ok && lit.Kind == token.INT {
+							if val := parseInt(lit.Value); val != nil {
+								field.ProtoField = val
+							}
+						}
+					}
+				case "Unique":
+					field.Unique = true
+				case "Default":
+					if len(e.Args) > 0 {
+						field.DefaultValue = parseDefaultValue(e.Args[0])
+					}
+				}
+
+				// Continue with the receiver of this method call
+				currentExpr = selExpr.X
+
+			} else {
+				// not a method call, check if it is a top level function call
+				if _, ok := e.Fun.(*ast.Ident); ok {
+					// Handle calls like entlite.String
+					break
+				}
+				currentExpr = nil
+			}
+		default:
+			currentExpr = nil
+		}
+	}
+
+	return field
+
+}
+
+func parseDefaultValue(expr ast.Expr) any {
+	switch e := expr.(type) {
+	case *ast.BasicLit:
+		switch e.Kind {
+		case token.STRING:
+			return strings.Trim(e.Value, "\"")
+		case token.INT:
+			var val int
+			fmt.Scanf(e.Value, "%d", &val)
+			return val
+		case token.FLOAT:
+			var val float32
+			fmt.Scanf(e.Value, "%f", &val)
+			return val
+		}
+	case *ast.Ident:
+		if e.Name == "true" {
+			return true
+		}
+		if e.Name == "false" {
+			return false
+		}
+	}
+	return nil
+}
+
+func parseInt(s string) *int {
+	var i int
+	if _, err := fmt.Scanf(s, "%d", &i); err == nil {
+		return &i
+	}
+	return nil
 }
 
 func parseAnnotationsMethod(funcDecl *ast.FuncDecl) ([]schema.Annotation, error) {
@@ -181,7 +331,7 @@ func parseMethodsArguments(expr ast.Expr) []schema.Method {
 			case "MethodList":
 				methods = append(methods, schema.MethodList)
 			}
-			// TODO
+			// TODO delete
 			fmt.Printf("Found methods: %s\n", selExpr.Sel.Name)
 		}
 	}
