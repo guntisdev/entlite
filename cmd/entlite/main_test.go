be73afecaae543c12cb232f62cb0021d2076522c
Guntis <guntis.smaukstelis@gmail.com>
2026-03-01 19:20:10 +0200

    Test new command not to have any extra files

--- cmd/entlite/main_test.go
+++ cmd/entlite/main_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -20,11 +22,40 @@ func TestNewCommandFunction(t *testing.T) {
 		"ent/sqlc.yaml",
 		"ent/buf.yaml",
 		"ent/buf.gen.yaml",
+		"ent/generate.go",
 	}
 
+	// Check all expected files exist
 	for _, file := range expectedFiles {
 		if _, err := os.Stat(file); os.IsNotExist(err) {
 			t.Errorf("Expected file %s was not created", file)
+		}
+	}
+
+	// Check no unexpected files were created
+	var actualFiles []string
+	err := filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			cleanPath := strings.TrimPrefix(filepath.ToSlash(path), "./")
+			actualFiles = append(actualFiles, cleanPath)
+		}
+		return nil
+	})
+	if err != nil {
+		t.Fatalf("Failed to walk directory: %v", err)
+	}
+
+	expectedMap := make(map[string]bool)
+	for _, file := range expectedFiles {
+		expectedMap[file] = true
+	}
+
+	for _, actual := range actualFiles {
+		if !expectedMap[actual] {
+			t.Errorf("Unexpected file created: %s", actual)
 		}
 	}
 }
