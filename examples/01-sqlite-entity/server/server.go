71196d0577412e7cc95798573912656e098dfbb7
Guntis <guntis.smaukstelis@gmail.com>
2026-03-30 21:17:45 +0300

    fix optional proto with buf.gen.yaml v1.34.2

--- examples/01-sqlite-entity/server/server.go
+++ examples/01-sqlite-entity/server/server.go
@@ -32,15 +32,13 @@ func (s *UserServer) Create(
 	queries := db.New(s.db)
 
 	userID, err := queries.CreateUser(ctx, db.CreateUserParams{
-		Email:    req.Msg.Email,
-		Name:     req.Msg.Name,
-		Age:      req.Msg.Age,
-		Password: req.Msg.Password,
-		// TODO fix to accept optional value
-		Score:   0.0,
-		Uuid:    req.Msg.Uuid,
-		IsAdmin: req.Msg.IsAdmin,
-		// TODO set apikey as optional in proto
+		Email:       req.Msg.Email,
+		Name:        req.Msg.Name,
+		Age:         req.Msg.Age,
+		Password:    req.Msg.Password,
+		Score:       *req.Msg.Score,
+		Uuid:        req.Msg.Uuid,
+		IsAdmin:     req.Msg.IsAdmin,
 		ApiKey:      &req.Msg.ApiKey,
 		LastLoginMs: req.Msg.LastLoginMs,
 	})
@@ -84,15 +82,13 @@ func (s *UserServer) Update(
 	queries := db.New(s.db)
 
 	dbUser, err := queries.UpdateUser(ctx, db.UpdateUserParams{
-		ID:       req.Msg.ID,
-		Email:    req.Msg.Email,
-		Name:     req.Msg.Name,
-		Age:      req.Msg.Age,
-		Password: req.Msg.Password,
-		// TODO fix to accept optional value
-		Score:   0.0,
-		IsAdmin: req.Msg.IsAdmin,
-		// TODO set apikey as optional in proto
+		ID:          req.Msg.ID,
+		Email:       req.Msg.Email,
+		Name:        req.Msg.Name,
+		Age:         req.Msg.Age,
+		Password:    req.Msg.Password,
+		Score:       *req.Msg.Score,
+		IsAdmin:     req.Msg.IsAdmin,
 		ApiKey:      &req.Msg.ApiKey,
 		LastLoginMs: req.Msg.LastLoginMs,
 	})
